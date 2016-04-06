c063f179ac952e7ecf63fef885382f9b67b7883f
trizer <mrtrizer@gmail.com>
2016-04-07 02:33:39 +0300

    Full initialization and building process.

--- scripts/init.py
+++ scripts/init.py
@@ -13,6 +13,10 @@ def run(argv, projectDir, engineDir):
                                         "Remove config.json to reinitialize.")
     projectDir = "."
 
+    print os.popen("git init").read().strip()
+    print os.popen("git submodule add https://github.com/mrtrizer/FlappyEngine.git engine").read().strip()
+    print os.popen("git submodule update --init").read().strip()
+
     templateDir = os.path.join(engineDir, "templates/project")
     projectName = argv[1]
     config = {"name": projectName}
