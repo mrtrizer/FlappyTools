5376fbf85f412df1b1426cf50b8890273d4b9888
trizer <mrtrizer@gmail.com>
2016-04-17 23:33:39 +0300

    Template project is updated

--- templates/project/src/world.cpp
+++ templates/project/src/world.cpp
@@ -11,7 +11,7 @@ void World::recalc(GObj::DeltaT, const GContext &) {
 }
 
 void World::init() {
-    sprite = getRoot()->ADD_CHILD(GAnimation,"bird",10,10,POS(-5,-5,1),2,0.5);
-    getRoot()->ADD_CHILD(GPresenterSprite,"background",200,200,POS(-100,-100,0));
-    setActiveCamera(getRoot()->ADD_CHILD(GObjCamera,100,1.0,500,POS(0,0,0)));
+    sprite = getRoot()->ADD_CHILD(GAnimation,"bird",10,10,GPos(-5,-5,1),2,0.5);
+    getRoot()->ADD_CHILD(GPresenterSprite,"background",200,200,GPos(-100,-100,0));
+    setActiveCamera(getRoot()->ADD_CHILD(GObjCamera,100,1.0,500,GPos(0,0,0)));
 }
