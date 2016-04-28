9b02a4826985b59393b94be358da83f51252750d
trizer <mrtrizer@gmail.com>
2016-04-29 02:42:48 +0300

    Improved web template. Added custom index.html without extra code.

--- templates/targets/web/build.sh
+++ templates/targets/web/build.sh
@@ -1,3 +1,3 @@
 emconfigure cmake
 emmake make
-emcc `find CMakeFiles -name "*.o"` engine/libFlappyEngine.so --preload-file ../../build/res/@/res/ --use-preload-plugins -o test.html
+emcc `find CMakeFiles -name "*.o"` engine/libFlappyEngine.so --preload-file ../../build/res/@/res/ --use-preload-plugins -o {!name!}.js
